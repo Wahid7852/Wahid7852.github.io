1b443f7c3e6a14a389f21f65c8788bdb66a4fbcb
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-08-15 15:07:09 +0400

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -14,12 +14,16 @@ const About = () => {
       <section>
         <h2 className="title-font pink-text h2-tag">About Me</h2>
         <p className="white-text p-tag">
-          After working in different fields for over 9 years and seeking
-          something special, I have recently found my passion in web
-          development, currently JavaScript and its frameworks. My latest job
-          included social media and management however I decided to give it a go
-          and dive into web developement field. I am looking forward to working
-          on real projects!
+          I am a front-end web developer with 9 years of working experience in
+          fields like sales, customer support, social media, and management. I
+          combine my management expertise with technical skills and fully commit
+          to consistency, meeting deadlines, and utilizing all available
+          resources to achieve results.I enjoy creating beautiful, accessible,
+          and responsive websites using HTML, CSS, Tailwind, Sass, SCSS, Vanilla
+          JavaScript, and React. Effective use of time, problem-solving, and
+          communication skills help me show a positive attitude in complex
+          situations, and my desire to learn quickly adapts me to changing
+          situations.
         </p>
         <h3 className="title-font white-text ">
           Here are a few tools & technologies I’ve been working with:
