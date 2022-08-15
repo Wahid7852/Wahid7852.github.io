a39ef43659e0331a10dd83b5c84d9af7b79b73ac
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-08-15 16:49:58 +0400

    updated projects

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -6,7 +6,6 @@ import Edgy from "../images/Edgy.jpeg";
 import RestCountries from "../images/RestCountries.jpeg";
 import Bankist from "../images/Bankist.jpeg";
 import FrontendMentorSolutions from "../images/FrontendMentorSolutions.jpeg";
-import Precious from "../images/Precious.png";
 
 const ProjectsPreviewData = [
   {
@@ -31,17 +30,6 @@ const ProjectsPreviewData = [
   },
   {
     id: 3,
-    img: Precious,
-    name: "Precious",
-    stack: ["< GraphQL />", "< React.js />", "< CSS3 />"],
-    src: "https://precious-store.vercel.app/",
-    source: "https://github.com/catherineisonline/precious",
-    description:
-      "Precious is an e-commerce website that has at least 150 women's clothing products with different sizes and color choices.",
-  },
-
-  {
-    id: 4,
     img: RestCountries,
     name: "Rest Countires",
     stack: ["< RESTful API />", "< CSS3 />", "< React.js />"],
@@ -51,7 +39,7 @@ const ProjectsPreviewData = [
       "REST Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
   },
   {
-    id: 5,
+    id: 4,
     img: Forkify,
     name: "Forkify Recipes",
     stack: ["< HTML5 />", "< SCSS />", "< Vanilla JS />", "< RESTful API />"],
@@ -61,7 +49,7 @@ const ProjectsPreviewData = [
       "Forkify is a vanilla JavaScript application that interacts with the Forkify API to fetch and display recipe food data. The user can search for a specific recipe, and save to a favorites list via local storage. The user can easily increase or decrease servings as per his need and can view detailed directions.",
   },
   {
-    id: 6,
+    id: 5,
     img: Bankist,
     name: "Bankist",
     stack: ["< Vanilla JS />", "< CSS3 />", "< HTML5 />"],
@@ -93,19 +81,9 @@ const ProjectsData = [
     description:
       "Travel with Catherine, a simple photography portfolio to show off some of my photography and code! Another great project to practice React and Tailwind.",
   },
-  {
-    id: 3,
-    img: Precious,
-    name: "Precious",
-    stack: ["< GraphQL />", "< React.js />", "< CSS3 />"],
-    src: "https://precious-store.vercel.app/",
-    source: "https://github.com/catherineisonline/precious",
-    description:
-      "Precious is an e-commerce website that has at least 150 women's clothing products with different sizes and color choices. ",
-  },
 
   {
-    id: 4,
+    id: 3,
     img: RestCountries,
     name: "Rest Countires",
     stack: ["< CSS3 />", "< React.js />", "< RESTful API />"],
@@ -115,7 +93,7 @@ const ProjectsData = [
       "REST Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
   },
   {
-    id: 5,
+    id: 4,
     img: Forkify,
     name: "Forkify Recipes",
     stack: ["< SCSS />", "< Vanilla JS />", "< RESTful API />"],
@@ -125,7 +103,7 @@ const ProjectsData = [
       "Forkify is a vanilla JavaScript application that interacts with the Forkify API to fetch and display recipe food data. The user can search for a specific recipe, and save to a favorites list via local storage. The user can easily increase or decrease servings as per his need and can view detailed directions.",
   },
   {
-    id: 6,
+    id: 5,
     img: Bankist,
     name: "Bankist",
     stack: ["< Vanilla JS />", "< CSS3 />", "< HTML5 />"],
@@ -136,7 +114,7 @@ const ProjectsData = [
   },
 
   {
-    id: 7,
+    id: 6,
     img: GuessMyNumber,
     name: "Guess My Number",
     stack: ["<HTML5 />", "< CSS3 />", "< Vanilla JS />"],
@@ -146,7 +124,7 @@ const ProjectsData = [
       "This JavaScript number guessing game project's main goal is to code a game where the user must guess a randomly generated number from 1 to 20. Depending on the number input, the user receives different messages until they guess the secret number.",
   },
   {
-    id: 8,
+    id: 7,
     img: FrontendMentorSolutions,
     name: "Frontend Mentor Solutions",
     stack: ["< Vue.js />", "< Sass />"],
@@ -157,7 +135,7 @@ const ProjectsData = [
       "Frontend Mentor provides designs so that you can practice front-end skills using a real-life workflow. Challenges include designs (both mobile and desktop), assets, and style requirements. Here is the collection of all my Frontend Mentor challenge solutions. You can find links to their repositories, live demos, and direct links to challenges on Frontend Mentor.",
   },
   {
-    id: 9,
+    id: 8,
     img: Edgy,
     name: "Edgy",
     stack: ["< Tailwind />", "< React.js />"],
