f394a1ef8a337da0427ef6da262dfeb56b710780
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-08-17 10:05:02 +0400

    updated folder

--- src/components/landingPage/AboutPreview.js
+++ src/components/landingPage/AboutPreview.js
@@ -1,5 +1,5 @@
-import AboutMe from "../about/AboutMe";
-import Stack from "../about/Stack";
+import AboutMe from "../About/AboutMe";
+import Stack from "../About/Stack";
 
 const AboutPreview = () => {
   return (
