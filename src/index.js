c2f8356d7fd3c15709708ce222abb925de7811d3
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-12 18:09:59 +0400

    fixing build folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./assets/styles/Styles.sass";
