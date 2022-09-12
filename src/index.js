54b09ace267f2684a1f26b2cdd9418913b27492f
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-12 17:53:59 +0400

    fixing about folder error with vercel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./assets/styles/Styles.sass";
 import Landing from "./components/landingPage/Landing.js";
 import Navigation from "./components/navigation/Navigation.js";
-import About from "./components/about/About";
+import About from "./components/about/AboutPage";
 import Portfolio from "./components/portfolio/Portfolio.js";
 import Contact from "./components/contact/Contact.js";
 import reportWebVitals from "./reportWebVitals";
