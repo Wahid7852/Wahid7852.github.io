0131f27586ef0cabea3831fd5e96fda207b4b524
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-30 20:08:52 +0400

    destructured sass files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  {Navigation}  from "./components/index.js";
 import { Landing, About, Portfolio, Contact } from "./routes/index.js";
 import "./core-ui/Styles.sass";
+import './routes/about/About.sass'
+import './routes/contact/Contact.sass'
+import './core-ui/Hover.sass'
+import './routes/landing/Landing.sass'
+import './routes/portfolio/Portfolio.sass'
+import './components/footer/Footer.sass'
+import './components/navigation/Navigation.sass'
 
 const App = () => {
     return (
